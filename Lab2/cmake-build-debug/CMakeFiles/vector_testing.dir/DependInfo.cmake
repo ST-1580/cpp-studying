cce6f7e7b7843edac2f1b629dd03198ca03a2883
Yaroslav Kirsanov <YarKirs@mail.ru>
2020-05-16 15:34:40 +0300

    First commit vector

---
+++ Lab2/cmake-build-debug/CMakeFiles/vector_testing.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/YarKirs/Documents/Programming/C++/cpp-studying/Lab2/gtest/gtest-all.cc" "C:/Users/YarKirs/Documents/Programming/C++/cpp-studying/Lab2/cmake-build-debug/CMakeFiles/vector_testing.dir/gtest/gtest-all.cc.obj"
+  "C:/Users/YarKirs/Documents/Programming/C++/cpp-studying/Lab2/gtest/gtest_main.cc" "C:/Users/YarKirs/Documents/Programming/C++/cpp-studying/Lab2/cmake-build-debug/CMakeFiles/vector_testing.dir/gtest/gtest_main.cc.obj"
+  "C:/Users/YarKirs/Documents/Programming/C++/cpp-studying/Lab2/main.cpp" "C:/Users/YarKirs/Documents/Programming/C++/cpp-studying/Lab2/cmake-build-debug/CMakeFiles/vector_testing.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab2/cmake-build-debug/CMakeFiles/vector_testing.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector_testing.dir/main.cpp.obj"
+  "CMakeFiles/vector_testing.dir/gtest/gtest-all.cc.obj"
+  "CMakeFiles/vector_testing.dir/gtest/gtest_main.cc.obj"
+  "vector_testing.pdb"
+  "vector_testing.exe"
+  "vector_testing.exe.manifest"
+  "libvector_testing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector_testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
