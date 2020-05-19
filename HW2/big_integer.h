80755cd2e1e4bf522a6b02e4746e1ca0dfd0952c
Yaroslav <steep1580@yandex.ru>
2020-05-19 10:46:27 +0300

    BIGINT added

--- HW2/big_integer.h
+++ HW2/big_integer.h
@@ -1,16 +1,25 @@
 #ifndef BIG_INTEGER_H
 #define BIG_INTEGER_H
 
-#include <cstddef>
-#include <iosfwd>
-#include <stdint-gcc.h>
 #include <vector>
+#include <string>
+#include <limits>
+#include <algorithm>
 
 struct big_integer
 {
+private:
+    void delete_zero();
+    void inverse(size_t sz);
+    friend uint32_t trial(big_integer &a, big_integer const &b);
+    friend bool smaller(big_integer const &a, big_integer const &b, size_t index);
+    friend void difference(big_integer &a, big_integer const &b, size_t index);
+
+public:
     big_integer();
     big_integer(big_integer const& other);
     big_integer(int a);
+    big_integer(uint32_t a);
     explicit big_integer(std::string const& str);
     ~big_integer();
 
@@ -39,6 +48,19 @@ struct big_integer
     big_integer& operator--();
     big_integer operator--(int);
 
+    friend big_integer operator+(big_integer a, big_integer const& b);
+    friend big_integer operator-(big_integer a, big_integer const& b);
+    friend big_integer operator*(big_integer a, big_integer const& b);
+    friend big_integer operator/(big_integer a, big_integer const& b);
+    friend big_integer operator%(big_integer a, big_integer const& b);
+
+    friend big_integer operator&(big_integer a, big_integer const& b);
+    friend big_integer operator|(big_integer a, big_integer const& b);
+    friend big_integer operator^(big_integer a, big_integer const& b);
+
+    friend big_integer operator<<(big_integer a, int b);
+    friend big_integer operator>>(big_integer a, int b);
+
     friend bool operator==(big_integer const& a, big_integer const& b);
     friend bool operator!=(big_integer const& a, big_integer const& b);
     friend bool operator<(big_integer const& a, big_integer const& b);
@@ -47,33 +69,14 @@ struct big_integer
     friend bool operator>=(big_integer const& a, big_integer const& b);
 
     friend std::string to_string(big_integer const& a);
+    std::string to_string(big_integer const& a);
 
 private:
     std::vector <uint32_t> value;
     bool sign;
 };
 
-big_integer operator+(big_integer a, big_integer const& b);
-big_integer operator-(big_integer a, big_integer const& b);
-big_integer operator*(big_integer a, big_integer const& b);
-big_integer operator/(big_integer a, big_integer const& b);
-big_integer operator%(big_integer a, big_integer const& b);
 
-big_integer operator&(big_integer a, big_integer const& b);
-big_integer operator|(big_integer a, big_integer const& b);
-big_integer operator^(big_integer a, big_integer const& b);
-
-big_integer operator<<(big_integer a, int b);
-big_integer operator>>(big_integer a, int b);
-
-bool operator==(big_integer const& a, big_integer const& b);
-bool operator!=(big_integer const& a, big_integer const& b);
-bool operator<(big_integer const& a, big_integer const& b);
-bool operator>(big_integer const& a, big_integer const& b);
-bool operator<=(big_integer const& a, big_integer const& b);
-bool operator>=(big_integer const& a, big_integer const& b);
-
-std::string to_string(big_integer const& a);
-std::ostream& operator<<(std::ostream& s, big_integer const& a);
+// std::ostream& operator<<(std::ostream& s, big_integer const& a);
 
 #endif // BIG_INTEGER_H
--- HW2/main.cpp
+++ HW2/main.cpp
@@ -2,7 +2,6 @@
 #include "big_integer.h"
 
 int main() {
-    big_integer a;
 
     return 0;
 }
